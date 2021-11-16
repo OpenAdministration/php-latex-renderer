0a302610b13e7242da9d6a1bbedafed95cd5aadd
Lukas Staab <l.staab@posteo.de>
2021-11-16 03:34:27 +0100

    softer escaping

--- src/LatexEscape.php
+++ src/LatexEscape.php
@@ -4,8 +4,11 @@ namespace PhpLatexRenderer;
 
 class LatexEscape
 {
-    public static function escape($twig, string $unsafe, $charset): string
+    public static function escape($twig, ?string $unsafe, $charset): string
     {
+        if ($unsafe === null) {
+            return '';
+        }
         $safer = str_replace(
             ['&', '%', '$', '#', '_', '{', '}', '~', '^', '\\'],
             ['\\&', '\\%', '\\$', '\\#', '\\_', '\\{', '\\}', '\\textasciitilde{}', '\\textasciicircum{}', '\\textbackslash{}'],
